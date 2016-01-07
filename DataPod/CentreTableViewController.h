b2caf3edf2ab031c9c9166415b7ed5bd0efb228d
fabiogeneral <fabiogeneral@gmail.com>
2016-01-07 15:37:56 -0500

    workspace set

--- DataPod/CentreTableViewController.h
+++ DataPod/CentreTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CentreTableViewController.h
 //  DataPod
 //
 //  Created by Fabio General on 1/7/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CentreTableViewController : UITableViewController
 
 @end
-
